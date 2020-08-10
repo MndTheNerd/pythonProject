9abebde123ae92b1386ed2c4520da418564b07a2
mndthenerd <mndthenerd@gmail.com>
2020-08-10 15:41:33 +0300

    learning python1

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ course = 'Python for "Beginners"'
 print(course)
 
 firstName = 'Mahmoud'
-lasName = 'Abbas'
+lastName = 'Abbas'
 
-msg = f'{firstName} {lasName} is learning Python!!'
+#print by charcter
+print(course[2])
+print(course[0:3])
+print(course[:])
+
+#unformatted string
+message = firstName + ' [' + lastName + '] is learning Python!'
+#to use fomatted string
+msg = f'{firstName} {lastName} is learning Python!!'
 print(msg)
+
+#to print the lenght of the string
+print(len(course))
